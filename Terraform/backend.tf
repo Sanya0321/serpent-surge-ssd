d031b1eb52881a06bf92f2da331b4a92a69f20a2
Sanya0321 <64781900+Sanya0321@users.noreply.github.com>
2025-08-20 15:08:29 +0200

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "s3terraform"
-    key = "s3terraform/statefolder/statefile"
+    bucket = "final-project-bucket-unique-name"
+    key = "final-project-bucket-unique-name/statefolder/statefile"
     region = "us-east-1"
     encrypt = true
   }
